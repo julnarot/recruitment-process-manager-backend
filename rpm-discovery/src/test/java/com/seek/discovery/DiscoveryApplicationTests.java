a8a7394c3d913402bf7be32e9d69212ee2eb5746
Julnarot <rauljhonatan.tola@gmail.com>
2025-12-14 00:23:30 -0500

    chore: discovery serve was initialized
    - now can implemented eureka server like discover
    
    - auth and customer service was linked on discovery server

---
+++ rpm-discovery/src/test/java/com/seek/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.seek.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
