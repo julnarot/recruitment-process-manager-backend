cbcaf69fc7352eced74d7cf06e34dce97c5fbf5f
Julnarot <rauljhonatan.tola@gmail.com>
2025-12-19 23:52:36 -0500

    fix: use correct entity on query

--- rpm-customer-ms/src/main/java/com/seek/rpm/customer/infrastructure/persistence/jpa/CustomerJpaRepository.java
+++ rpm-customer-ms/src/main/java/com/seek/rpm/customer/infrastructure/persistence/jpa/CustomerJpaRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-// import com.seek.rpm.customer.dto.CustomerMetricsDTO;
 
 @Repository
 public interface CustomerJpaRepository extends JpaRepository<CustomerJpaEntity, Long> {
@@ -13,7 +12,7 @@ public interface CustomerJpaRepository extends JpaRepository<CustomerJpaEntity, 
                 SELECT
                     AVG(c.age),
                     STDDEV_POP(c.age)
-                FROM Customer c
+                FROM CustomerJpaEntity c
             """)
     Object[] getCustomerMetrics();
 
